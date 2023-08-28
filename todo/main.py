4413ec8fb423dfc5143e4b409ee2b9c6bea5bb35
toa1102 <toatoauehara@gmail.com>
2023-08-28 23:22:26 +0900

    new

--- todo/main.py
+++ todo/main.py
@@ -10,18 +10,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.sqlite'
 #データーベースを作成
 db = SQLAlchemy(app)
 class Task(db.Model):
+    
     __tablename__ = "tasks"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text = db.Column(db.Text())
     status = db.Column(db.Integer)
     
-db.create_all()
+#テキストと違う[ 追加：with app.app_context(): ]
+with app.app_context():
+    db.create_all()
 
 #ルーティングとindexにアクセスされた場合の処理を記述
 @app.route('/')
 def index():
     tasks = Task.query.all()
-    return render_template('index.html', tasks = tasks)
+    return render_template("index.html", tasks = tasks)
 
 #app.run()でアプリケーションを起動する！
 app.run(debug=True, host=os.getenv('APP_ADDRESS', 'localhost'), port=8001)
